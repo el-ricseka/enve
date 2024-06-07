a8ba5e90946cba074d9028c02af60efe7d0e7021
Eric Sekamatte <sekamatteeric@gmail.com>
2024-06-07 21:21:04 +0300

    Removed Comments from index file

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <!-- <Hero /> -->
-    <!-- <About /> -->
-    <!-- <Services /> -->
-    <!-- <Banner /> -->
-    <!-- <Projects /> -->
-    <!-- <WhyUs /> -->
-    <!-- <Clients /> -->
+    <Hero />
+    <About />
+    <Services />
+    <Banner />
+    <Projects />
+    <WhyUs />
+    <Clients />
     <Footer />
   </div>
 </template>
